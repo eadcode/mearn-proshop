d342094600f44cce50c197c6ec32b31e842d1c29
Eric Durocher <erciduro@gmail.com>
2024-02-06 20:22:21 -0500

    Add header, footer and styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
-import './App.css'
+import { Container } from 'react-bootstrap';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
 
 function App() {
-  return (
-    <>
+    return (
+        <>
+            <Header />
 
-    </>
-  )
+            <main className="py-3">
+                <Container>
+                    <h1>Welcome to ProShop</h1>
+                </Container>
+            </main>
+
+            <Footer />
+        </>
+    );
 }
 
-export default App
+export default App;
